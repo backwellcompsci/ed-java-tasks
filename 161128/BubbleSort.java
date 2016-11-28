46c0d3e68555b09afda9d654350abdcd744152ed
Edward Callow <edward@edwardcallow.co.uk>
2016-11-28 10:08:39 +0000

    stupid java

---
+++ 161128/BubbleSort.java
@@ -0,0 +1,36 @@
+/*
+ * BubbleSort
+ * Edward Callow
+ * 161128
+ */
+
+public class BubbleSort{
+	public static void main(String[] args) {
+		int intArray[] = {9,8,7,5,6};
+		System.out.println("Before sorting");
+
+		for (int i :intArray) {
+			System.out.println(i);
+		}
+
+		BubbleSort(intArray);
+		System.out.println("After sorting");
+
+		for (int i :intArray) {
+			System.out.println(i);
+		}
+	}
+
+	private static void BubbleSort(int[] intArray) {
+		int temp = 0;
+		for (int i = 0; i <= intArray.length; i++) {
+			for (int j = 1; j < (intArray.length)-i; j++) {
+				if (intArray[j-1] > intArray[j]) {
+					temp = intArray[j-1];
+					intArray[j-1] = intArray[j];
+					intArray[j] = temp;
+				}
+			}
+		}
+	}
+}
