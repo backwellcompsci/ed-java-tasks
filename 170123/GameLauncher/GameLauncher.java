213b98b732075068da7f19177c1aa9a111d2fbe3
Edward Callow <edward@edwardcallow.co.uk>
2017-01-23 11:41:25 +0000

    not quite done

---
+++ 170123/GameLauncher/GameLauncher.java
@@ -0,0 +1,12 @@
+/*
+ * GameLauncher
+ * Edward Callow
+ * 170123
+ */
+
+public class GameLauncher {
+	public static void main(String[] args) {
+		GuessGame game = new GuessGame();
+		game.startGame();
+	}
+}
