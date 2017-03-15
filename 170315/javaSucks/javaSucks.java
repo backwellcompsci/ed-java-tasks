d9b0074b39b1496eba01c3f5a58879862f2204e6
Edward Callow <edward@edwardcallow.co.uk>
2017-03-15 18:20:53 +0000

    Java sucks.

---
+++ 170315/javaSucks/javaSucks.java
@@ -0,0 +1,14 @@
+/*
+ * javaSucks
+ * Edward Callow
+ * 1700315
+ */
+
+public class javaSucks {
+	public static void main(String[] args) {
+		boolean doesItSuck = true;
+		while (doesItSuck == true) {
+			System.out.println("Java sucks.");
+		}
+	}
+}
